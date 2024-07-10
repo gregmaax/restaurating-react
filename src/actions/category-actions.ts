a750c5c915bdd56501ac5c4efbd6a0537b50a4a5
gregmaax <greg.d.972@gmail.com>
2024-07-10 21:08:51 +0200

    complete server action to add data to the database

--- src/actions/category-actions.ts
+++ src/actions/category-actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { CategorySchema } from "~/schemas";
+import { db } from "~/server/db";
+import { categories } from "~/server/db/schema";
 
 export const createCategory = async (
   values: z.infer<typeof CategorySchema>,
@@ -11,6 +14,13 @@ export const createCategory = async (
   if (!validatedFields.success) {
     return { error: "Erreur ! Champs invalides" };
   }
+
+  await db.insert(categories).values({
+    name: values.name,
+    description: values.description,
+  });
+
+  revalidatePath("/");
 
   return { success: "Votre catégorie a bien été enregistrée !" };
 };
