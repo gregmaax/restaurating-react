253ca83c5deea11e2daed9d28498d7efc8b801ca
gregmaax <greg.d.972@gmail.com>
2024-11-14 20:46:53 +0100

    modification metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ import { Toaster } from "~/components/ui/sonner";
 
 export const metadata = {
   title: "Restaurating",
-  description: "Next.js playground for a restaurant app",
+  description:
+    "Ajoutez, notez, et retrouvez facilement vos restaurants favoris.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
@@ -22,6 +23,23 @@ export default async function RootLayout({
   return (
     <SessionProvider session={session}>
       <html lang="fr" className={`${GeistSans.variable} bg-panda-light-yellow`}>
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <title>Restaurating</title>
+        </head>
         <body>
           <main>{children}</main>
           <Toaster richColors />
